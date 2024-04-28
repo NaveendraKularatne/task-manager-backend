d2a4f5c6e27434fe264d2d5c91a39e81f63eb572
MOBITEL\NAVEENDRAK <naveendrak@mobitel.lk>
2024-04-28 17:50:15 +0530

    Create task entity

--- taskmanager/src/main/java/com/osos/taskmanager/entity/Task.java
+++ taskmanager/src/main/java/com/osos/taskmanager/entity/Task.java
@@ -1,7 +1,31 @@
 package com.osos.taskmanager.entity;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.sql.Date;
 
 @Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "task")
 public class Task {
+
+    @Id
+    private Long id;
+
+    @Column(name = "title", length = 100)
+    private String title;
+
+    @Column(name = "description", length = 255)
+    private String description;
+
+    @Column(name = "date")
+    private Date date;
 }
