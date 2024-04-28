24f2d12c35d8650ad8a5dabde35425484179309d
MOBITEL\NAVEENDRAK <naveendrak@mobitel.lk>
2024-04-29 01:48:35 +0530

    Add validations

--- taskmanager/src/main/java/com/osos/taskmanager/dto/TaskRequestDto.java
+++ taskmanager/src/main/java/com/osos/taskmanager/dto/TaskRequestDto.java
@@ -1,19 +1,27 @@
 package com.osos.taskmanager.dto;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
+import jakarta.validation.constraints.FutureOrPresent;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import java.sql.Date;
 import java.time.LocalDate;
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 public class TaskRequestDto {
+    @NotBlank(message = "Title is required")
     private String title;
+
+    @NotBlank(message = "Description is required")
     private String description;
+
+    @NotNull(message = "Due date is required")
+    @FutureOrPresent(message = "Due date must be in the future or present")
     @JsonFormat(pattern = "yyyy-MM-dd")
     private LocalDate duedate;
 }
