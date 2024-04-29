79ccfc0e64152b17d2a721cbfb3184a7d7d8012b
MOBITEL\NAVEENDRAK <naveendrak@mobitel.lk>
2024-04-29 18:21:05 +0530

    Add cross origin annotation

--- taskmanager/src/main/java/com/osos/taskmanager/controller/TaskController.java
+++ taskmanager/src/main/java/com/osos/taskmanager/controller/TaskController.java
@@ -3,7 +3,6 @@ package com.osos.taskmanager.controller;
 import com.osos.taskmanager.dto.TaskRequestDto;
 import com.osos.taskmanager.dto.TaskResponseDto;
 import com.osos.taskmanager.service.TaskService;
-import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -12,6 +11,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/tasks")
+@CrossOrigin
 public class TaskController {
 
     private final TaskService taskService;
