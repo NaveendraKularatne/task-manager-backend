7f5ce0e2411c13520fa18dc7e15fe3019744d137
MOBITEL\NAVEENDRAK <naveendrak@mobitel.lk>
2024-05-05 23:50:07 +0530

    Handle invalid credentials

--- taskmanager/src/main/java/com/osos/taskmanager/controller/AuthController.java
+++ taskmanager/src/main/java/com/osos/taskmanager/controller/AuthController.java
@@ -3,7 +3,9 @@ package com.osos.taskmanager.controller;
 import com.osos.taskmanager.config.UserAuthProvider;
 import com.osos.taskmanager.dto.UserInfoRequestDto;
 import com.osos.taskmanager.dto.UserInfoResponseDto;
+import com.osos.taskmanager.error.ErrorResponse;
 import com.osos.taskmanager.service.UserInfoService;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -21,9 +23,13 @@ public class AuthController {
     }
 
     @PostMapping("/login")
-    public ResponseEntity<UserInfoResponseDto> login(@RequestBody UserInfoRequestDto userInfoRequestDto) {
-        UserInfoResponseDto userInfoResponseDto = this.userInfoService.login(userInfoRequestDto);
-        userInfoResponseDto.setToken(this.userAuthProvider.createToken(userInfoRequestDto));
-        return ResponseEntity.ok().body(userInfoResponseDto);
+    public ResponseEntity<?> login(@RequestBody UserInfoRequestDto userInfoRequestDto) {
+        try {
+            UserInfoResponseDto userInfoResponseDto = this.userInfoService.login(userInfoRequestDto);
+            userInfoResponseDto.setToken(this.userAuthProvider.createToken(userInfoRequestDto));
+            return ResponseEntity.ok().body(userInfoResponseDto);
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(new ErrorResponse("Invalid credentials"));
+        }
     }
 }
